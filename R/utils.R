3d8296092fa56f71af6d854045c6a18fa5767537
Tyson S. Barrett <t.barrett88@gmail.com>
2019-10-22 17:12:01 -0600

    updated utils

--- R/utils.R
+++ R/utils.R
@@ -8,10 +8,6 @@
 .check_pdf <- function(pdf){
   # Stop if null
   if (is.null(pdf))
-    stop("Something went wrong, likely in the yaml. Does it have all the necessary components? ", call. = FALSE)
-
-  # Stop if not pandoc to latex
-  if (pdf$pandoc$to == "latex")
-    stop("Something went wrong, likely in the yaml. Did you change something in the output part of the yaml? ", call. = FALSE)
+    stop("Something went wrong, possibly in the yaml. Does it have all the necessary components? ", call. = FALSE)
 }
 
